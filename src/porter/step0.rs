26612541e04155f693b2b34f0d3a46722ca17fb4
Dominik.Kotecki <dominikus1910@gmail.com>
2023-04-02 17:06:21 +0000

    add apply method

--- src/porter/step0.rs
+++ src/porter/step0.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 fn trim_end_apostrophe(str: String) -> String {
     match str.strip_suffix('\'') {
         Some(word) => word.to_string(),
@@ -17,6 +19,26 @@ fn trim_s_apostrophe(str: String) -> String {
         Some(word) => word.to_string(),
         None => str
     }
+}
+
+fn mark_consonant_y(word: String) -> String {
+    let re = Regex::new(r"^y|([aeiou])y").unwrap();
+
+    if word.contains('y') {
+        re.replace_all(&word, |caps: &regex::Captures| {
+            if let Some(mat) = caps.get(1) {
+                format!("{}Y", mat.as_str())
+            } else {
+                "Y".to_string()
+            }
+        }).to_string()
+    } else {
+        word
+    }
+}
+
+pub fn apply(word: String) -> String {
+    mark_consonant_y(trim_s_apostrophe(trim_end_apostrophe(trim_start_apostrophe(word))))
 }
 
 #[cfg(test)]
@@ -43,10 +65,28 @@ mod tests {
     }
 
     #[test]
-    fn test_trim_s_apostrophe() {
+    fn test_mark_consonant_y() {
         let array = vec![("te'st", "te'st"), ("test's", "test")];
         for (word, expected) in array {
             let subject = trim_s_apostrophe(word.to_string());
+            assert_eq!(expected.to_string(), subject);
+        }
+    }
+
+    #[test]
+    fn test_trim_s_apostrophe() {
+        let array = vec![("flying", "flying"), ("youth", "Youth"), ("boyish", "boYish")];
+        for (word, expected) in array {
+            let subject = mark_consonant_y(word.to_string());
+            assert_eq!(expected.to_string(), subject);
+        }
+    }
+
+    #[test]
+    fn test_apply() {
+        let array = vec![("youth's'", "Youth")];
+        for (word, expected) in array {
+            let subject = apply(word.to_string());
             assert_eq!(expected.to_string(), subject);
         }
     }
