50df170790f16af3432ba4558806cf9f8940bb70
Dominik.Kotecki <dominikus1910@gmail.com>
2023-04-03 17:28:47 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod  porter;
 
 fn main() {
+    let slice = &"Hello, world!"[0..];
     println!("not fast enough flash");
-    println!("Hello, world!");
+    println!("Hello, world! {}", slice);
 }
--- src/porter/step1a.rs
+++ src/porter/step1a.rs
@@ -1,16 +1,38 @@
+use std::ops::Add;
+
 enum ReplaceResult {
     Found(String),
     Next(String),
 }
 
 fn replace_suffix(str: String, txt: &str, replacement: &str) -> String {
-    ""
+    if str.ends_with(txt) {
+        let slice = &str[0..(str.len() - txt.len())];
+        let result = String::from(slice).add(replacement);
+        return result
+    }
+    str
 } 
 
 fn replace_sses(str: String) -> ReplaceResult {
     if str.ends_with("sses") {
-        let result = replace_suffix(str, "sses", "ss");
+        let result = replace_suffix(str.clone(), "sses", "ss");
         ReplaceResult::Found(result);
     }
     ReplaceResult::Next(str)
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_replace_suffix() {
+        let array = vec![("test", "st", "xt", "text")];
+        for (word, txt, replacement, expected) in array {
+            let subject = replace_suffix(word.to_string(), txt, replacement);
+            assert_eq!(expected.to_string(), subject);
+        }
+    }
 }
