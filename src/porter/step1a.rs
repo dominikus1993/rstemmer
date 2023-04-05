55ac30857381f12e13554e6f0c2fc61de365b4a0
Dominik.Kotecki <dominikus1910@gmail.com>
2023-04-05 16:30:19 +0000

    .

--- src/porter/step1a.rs
+++ src/porter/step1a.rs
@@ -1,8 +1,18 @@
 use std::ops::Add;
 
+#[derive(PartialEq, Eq, Debug, Clone)]
 enum ReplaceResult {
     Found(String),
     Next(String),
+}
+
+impl ReplaceResult {
+    fn bind(&self, f: fn(String) -> ReplaceResult) -> ReplaceResult {
+        match self {
+            Self::Found(_) => self.clone(),
+            Self::Next(str) => f(str.clone())
+        }
+    }
 }
 
 fn replace_suffix(str: String, txt: &str, replacement: &str) -> String {
@@ -17,9 +27,16 @@ fn replace_suffix(str: String, txt: &str, replacement: &str) -> String {
 fn replace_sses(str: String) -> ReplaceResult {
     if str.ends_with("sses") {
         let result = replace_suffix(str.clone(), "sses", "ss");
-        ReplaceResult::Found(result);
+        return ReplaceResult::Found(result);
     }
     ReplaceResult::Next(str)
+}
+
+fn leave_us_and_ss(word: String) -> ReplaceResult {
+    if word.ends_with("ss") || word.ends_with("us") {
+        return ReplaceResult::Found(word);
+    }
+    return ReplaceResult::Next(word);
 }
 
 #[cfg(test)]
@@ -28,11 +45,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_replace_suffix() {
-        let array = vec![("test", "st", "xt", "text")];
-        for (word, txt, replacement, expected) in array {
-            let subject = replace_suffix(word.to_string(), txt, replacement);
-            assert_eq!(expected.to_string(), subject);
+    fn test_replace_sses() {
+        let array = vec![("actresses", ReplaceResult::Found("actress".to_string()))];
+        for (word, expected) in array {
+            let subject = replace_sses(word.to_string());
+            assert_eq!(expected, subject);
+        }
+    }
+
+    #[test]
+    fn test_leave_us_and_ss() {
+        let array = vec![("abyss", ReplaceResult::Found("abyss".to_string())), ("us", ReplaceResult::Found("us".to_string())), ("gap", ReplaceResult::Next("gap".to_string()))];
+        for (word, expected) in array {
+            let subject = leave_us_and_ss(word.to_string());
+            assert_eq!(expected, subject);
         }
     }
 }
